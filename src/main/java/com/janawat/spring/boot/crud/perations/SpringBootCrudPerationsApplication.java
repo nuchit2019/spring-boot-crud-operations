058d4975fb06c3e28e107ac47cb64f4eba5dacef
Nuchit Atjanawat <nuchit@outlook.com>
2022-02-18 14:31:20 +0700

    first commit

---
+++ src/main/java/com/janawat/spring/boot/crud/perations/SpringBootCrudPerationsApplication.java
@@ -0,0 +1,13 @@
+package com.janawat.spring.boot.crud.perations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudPerationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudPerationsApplication.class, args);
+	}
+
+}
